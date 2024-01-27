b4a6d05232189bf91af29b474121b076280ac570
kalashpatil33 <patilkalash33@gmail.com>
2024-01-27 09:51:26 +0530

    Added Validation checks while signIn and signUp

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,57 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Header from './Header'
-
+import { checkValidData } from '../utils/validate';
 const Login = () => {
   const [isSignInForm, setisSignInForm] = useState(true);
+  const [errorMessage, seterrorMessage] = useState(null);
+  const email = useRef(null);
+  const name = useRef(null);
+  const password = useRef(null);
+  const nameRegex = /^[a-zA-Z]+(?:\s[a-zA-Z]+)?$/;
+
   const toggleSignIn = () => {
     setisSignInForm(!isSignInForm); //
+  }
+  const handleButtonClick = () => {
+    //validate the form data
+    // checkValidData();
+    // console.log(email);
+    // console.log(password);
+    const enteredName = name.current ? name.current.value : '';
+    const enteredEmail = email.current ? email.current.value : '';
+    const enteredPassword = password.current ? password.current.value : '';
+    if (!isSignInForm && !nameRegex.test(enteredName)) {
+      seterrorMessage('Invalid name format. Please enter a valid name.');
+      return;
+    }
+    const message = checkValidData(enteredEmail, enteredPassword);
+    // console.log(message)
+    seterrorMessage(message);
   }
   return (
     <div>
       <Header />
-      <div className='absolute'><img src="https://assets.nflxext.com/ffe/siteui/vlv3/9134db96-10d6-4a64-a619-a21da22f8999/a449fabb-05e4-4c8a-b062-b0bec7d03085/IN-en-20240115-trifectadaily-perspective_alpha_website_large.jpg" alt='Background' /></div>
-      <form className='w-3/12 absolute p-12 bg-black bg-opacity-80 my-36 mx-auto right-0 left-0 text-white rounded-lg'>
+      <div className='absolute'><img src="https://assets.nflxext.com/ffe/siteui/vlv3/9134db96-10d6-4a64-a619-a21da22f8999/a449fabb-05e4-4c8a-b062-b0bec7d03085/IN-en-20240115-trifectadaily-perspective_alpha_website_large.jpg" alt='Background' />
+      </div>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+      }}
+        className='w-3/12 absolute p-12 bg-black bg-opacity-80 my-36 mx-auto right-0 left-0 text-white rounded-lg'>
         <h1 className='font-bold text-3xl py-4 m-2 '>{isSignInForm ? 'Sign In' : 'Sign Up'}</h1>
-        {!isSignInForm && <input type="text" placeholder='Name' className='p-4 my-4 w-full bg-gray-700 rounded-lg' />}
-        <input type="email" placeholder='Email Address' className='p-4 my-4 w-full bg-gray-700 rounded-lg' />
-
-        <input type="password" placeholder='Password' className='p-4 my-4 w-full bg-gray-700 rounded-lg' />
-        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">{isSignInForm ? 'Sign In' : 'Sign Up'} </button>
+        {!isSignInForm && <input ref={name}
+          type="text" placeholder='Name' className='p-4 my-4 w-full bg-gray-700 rounded-lg' />}
+        <input
+          ref={email}
+          type="email" placeholder='Email Address' className='p-4 my-4 w-full bg-gray-700 rounded-lg' />
+        <input
+          ref={password}
+          type="password" placeholder='Password' className='p-4 my-4 w-full bg-gray-700 rounded-lg' />
+        <p className='text-red-500 font-bold text-lg'>{errorMessage}</p>
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg"
+          onClick={handleButtonClick}>{isSignInForm ? 'Sign In' : 'Sign Up'} </button>
         <p className='p-4 cursor-pointer' onClick={toggleSignIn}>{isSignInForm ? 'New to Netflix? Sign Up Now' : 'Already Registerd? Sign In'}</p>
       </form>
     </div>
   )
 }
-
 export default Login
